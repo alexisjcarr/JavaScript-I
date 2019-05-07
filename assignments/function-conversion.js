3f4a5d70f791dcea8b4f717a0a6b0d43c18be87b
alexisjcarr <acarr59@gmail.com>
2019-05-06 19:05:02 -0500

    Formatting

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -1,36 +1,35 @@
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
 
 const myFunction = () => {
-    console.log("Function was invoked!");
+  console.log("Function was invoked!");
 };
 
 myFunction();
 
 const anotherFunction = param => {
-    return param;
+  return param;
 };
 
 console.log(anotherFunction("Example"));
 
 const add = (param1, param2) => {
-    return param1 + param2;
+  return param1 + param2;
 };
 
-console.log(add(1,2));
+console.log(add(1, 2));
 
 const subtract = (param1, param2) => {
-    return param1 - param2;
+  return param1 - param2;
 };
 
-console.log(subtract(1,2));
-
+console.log(subtract(1, 2));
 
 // Stretch
 
-exampleArray = [1,2,3,4];
+exampleArray = [1, 2, 3, 4];
 
 const triple = exampleArray.map(num => {
-    return num * 3;
+  return num * 3;
 });
 
 console.log(triple);
